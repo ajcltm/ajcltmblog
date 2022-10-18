7f1d9c0449513596b9d3564c9bd40511cdcf0c91
ajcltm <ajcltm@gmail.com>
2022-10-19 01:39:33 +0900

    2022.10.19. 1:39

--- src/components/nav/ArchiveNav.tsx
+++ src/components/nav/ArchiveNav.tsx
@@ -9,19 +9,52 @@ type Contents = {
 type ArchiveNavProps = {
     admin : boolean;
     contents : Array<Contents>
+    currentContent : Contents
+    setCurrentContentId : React.Dispatch<React.SetStateAction<string>>
+    newContent : ()=>void
+
 };
 
-function ArchiveNav(props:ArchiveNavProps) {
+function ArchiveHeader() {
+    return (
+        <div className="archive-nav-header">
+            <h4>Category Add</h4>
+            <button className='archive-nav-add-button'> + </button>
+        </div>
+    )
+}
+
+function AdminArchiveNav(props:ArchiveNavProps) {
     const inputElement = props.contents.map(content=> {
-            return <input type='text' value={content.title} />
-    })
+        return <input className='archiveNav-input' type='text' value={content.title} />
+})
+    return (
+        props.contents.length > 0 ?
+        <div className='archive-nav-wraper'>
+            {inputElement}
+        </div>
+        :
+        <div className="archive-nav-wraper"></div> 
+    )
+}
+
+function ArchiveNav(props:ArchiveNavProps) {
     return (
         props.admin ?
-        (props.contents.length > 0 ?
-            <div>{inputElement}</div>
-            : <input type='text' value='empty'/> )
+            <div className='archive-nav'>  
+                <ArchiveHeader />
+                <AdminArchiveNav 
+                admin={props.admin} 
+                contents={props.contents} 
+                currentContent={props.currentContent} 
+                setCurrentContentId={props.setCurrentContentId} 
+                newContent={props.newContent} 
+                />
+            </div>
         :
         <a href=""></a>
+
+
         // props.admin ?
         // (props.contents.length > 0 ?
         //     props.contents.map(content=> {
@@ -33,5 +66,7 @@ function ArchiveNav(props:ArchiveNavProps) {
         //         <div></div>)
     );
 };
+
+
 
 export default ArchiveNav;
--- src/routes/ArchiveApp.tsx
+++ src/routes/ArchiveApp.tsx
@@ -6,6 +6,7 @@ import Content from '../components/Content';
 import Aside from '../components/Aside';
 import Advertise from '../components/Advertise';
 import {BrowserRouter as Router, Routes, Route, Outlet} from "react-router-dom";
+import {nanoid} from 'nanoid'
 
 function ArchiveApp() {
   type Contents = {
@@ -13,11 +14,47 @@ function ArchiveApp() {
     title: string;
     content: string;
 }
-  // const Contents = [{id:'0001', 'title': 'newContent', 'content':'This is the content!'}]
-  const Contents: Contents[] = []
+  // const contents = [{id:'0001', 'title': 'newContent', 'content':'This is the content!'}]
+  // const Contents: Contents[] = []
+
+  const [contents, setContents] = React.useState<Contents[]>([])
+  const [currentContentId, setCurrentContentId] = React.useState(
+    (contents[0] && contents[0].id) || ""
+  )
+
+  function ceateNewContent() {
+    const newContent : Contents = {
+      id: nanoid(),
+      title: 'New Category',
+      content: 'This is the new content!'
+    }
+    setContents(prevContents => [newContent, ...prevContents])
+    setCurrentContentId(newContent.id)
+  }
+
+  function updateNote(text:string) {
+    setContents(oldContents => oldContents.map(oldContent => {
+      return oldContent.id === currentContentId
+        ? {...oldContent, content:text}
+        : oldContent
+    }))
+  }
+
+  function findCurrentContent() {
+    return contents.find(content => {
+      return content.id === currentContentId
+    }) || contents[0]
+  }
+
   return (
     <div className='screen-grid'>
-      <ArchiveNav admin={true} contents={Contents} />
+      <ArchiveNav 
+        admin={true} 
+        contents={contents} 
+        currentContent={findCurrentContent()} 
+        setCurrentContentId={setCurrentContentId} 
+        newContent={ceateNewContent}
+      />
       <Outlet />
       <Aside />
       <Advertise />
